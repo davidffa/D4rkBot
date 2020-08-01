490503414eb58d5075f4b71fc8d0549faba1e411
David <davidfilipeamorim@gmail.com>
2020-08-01 20:12:05 +0100

    Render command

--- commands/render.js
+++ commands/render.js
@@ -1,5 +1,7 @@
-const { MessageEmbed } = require('discord.js');
-const canvas = require('canvas');
+const { MessageEmbed, MessageAttachment } = require('discord.js');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
     name: 'render',
@@ -9,19 +11,47 @@ module.exports = {
     usage: '<URL>',
     cooldown: 3,
     async execute(client, message, args, prefix) {
-        if (message.author.id != '334054158879686657')
-            return message.channel.send('Comando em manutenção!');
-
         if (!args.length) 
             return message.channel.send(`:x: Argumentos em falta, **Usa:** ${prefix}render <URL>`);
 
-        canvas.loadImage(args[0]).then((img) => {
-            const embed = new MessageEmbed()
-                .setTitle(`(${args[0]})[${args[0]}]`)
-                .setImage(img)
-                .setFooter(`${message.author.tag}`, message.author.displayAvatarURL())
-                .setTimestamp();
-            message.channel.send(embed);
-        }).catch(message.channel.send(':x: Link inválido!'));
+        const name = 'screenshot' + Math.floor((Math.random() * 100) + 1);
+        let url;
+
+        if (!args[0].startsWith('http')) 
+            url = 'http://' + args[0];
+        else 
+            url = args[0];
+        
+        
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+            deviceScaleFactor: 1
+        });
+
+        try {
+            await page.goto(url);
+        }catch (err) {
+            return message.channel.send(':x: Link inválido!');
+        }
+        
+        await page.screenshot({ path: `./screenshots/${name}.png`});
+        await browser.close();
+
+        const attachment = new MessageAttachment(`./screenshots/${name}.png`);
+        
+        const embed = new MessageEmbed()
+            .setTitle(args[0])
+            .setURL(url)
+            .setImage(`attachment://${name}.png`)
+            .setFooter(`${message.author.tag}`, message.author.displayAvatarURL())
+            .setTimestamp();
+
+        await message.channel.send({ embed, files: [attachment] });
+
+        fs.unlinkSync(`./screenshots/${name}.png`);
     }
 }
