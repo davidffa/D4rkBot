2ca350d7f17abf1787d1c5da880b82ad44eb7e3b
David <davidfilipeamorim@gmail.com>
2020-08-01 23:38:19 +0100

    Re-added render command

---
+++ commands/render.js
@@ -0,0 +1,62 @@
+const { MessageEmbed, MessageAttachment } = require('discord.js');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    name: 'render',
+    description: 'Renderiza uma página web',
+    aliases: ['webrender'], 
+    category: 'Outros',
+    usage: '<URL>',
+    cooldown: 3,
+    async execute(client, message, args, prefix) {
+        if (!args.length) 
+            return message.channel.send(`:x: Argumentos em falta, **Usa:** ${prefix}render <URL>`);
+
+        const name = 'screenshot' + Math.floor((Math.random() * 100) + 1);
+        let url;
+
+        if (!args[0].startsWith('http')) 
+            url = 'http://' + args[0];
+        else 
+            url = args[0];
+        
+        
+        const browser = await puppeteer.launch({
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+            ]
+        });
+        const page = await browser.newPage();
+
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+            deviceScaleFactor: 1
+        });
+
+        try {
+            await page.goto(url);
+        }catch (err) {
+            return message.channel.send(':x: Link inválido!');
+        }
+        
+        await page.screenshot({ path: `./screenshots/${name}.png`});
+        await browser.close();
+
+        const attachment = new MessageAttachment(`./screenshots/${name}.png`);
+        
+        const embed = new MessageEmbed()
+            .setTitle(args[0])
+            .setURL(url)
+            .setImage(`attachment://${name}.png`)
+            .setFooter(`${message.author.tag}`, message.author.displayAvatarURL())
+            .setTimestamp();
+
+        await message.channel.send({ embed, files: [attachment] });
+
+        fs.unlinkSync(`./screenshots/${name}.png`);
+    }
+}
