044473ceb8b6a9528bca6ebc1836ffacacdfbf0b
David <davidfilipeamorim@gmail.com>
2020-08-02 01:19:23 +0100

    Bug fix

--- commands/render.js
+++ commands/render.js
@@ -1,7 +1,6 @@
 const { MessageEmbed, MessageAttachment } = require('discord.js');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const path = require('path');
 
 module.exports = {
     name: 'render',
@@ -14,6 +13,8 @@ module.exports = {
         if (!args.length) 
             return message.channel.send(`:x: Argumentos em falta, **Usa:** ${prefix}render <URL>`);
 
+        if (message.author.id != '334054158879686657')
+            return message.channel.send(':x: Em manutenção!');
         const name = 'screenshot' + Math.floor((Math.random() * 100) + 1);
         let url;
 
@@ -21,8 +22,8 @@ module.exports = {
             url = 'http://' + args[0];
         else 
             url = args[0];
-        
-        
+
+
         const browser = await puppeteer.launch({
             args: [
                 '--no-sandbox',
@@ -30,33 +31,33 @@ module.exports = {
             ]
         });
         const page = await browser.newPage();
-
+    
         await page.setViewport({
             width: 1920,
             height: 1080,
             deviceScaleFactor: 1
         });
-
+    
         try {
             await page.goto(url);
         }catch (err) {
             return message.channel.send(':x: Link inválido!');
         }
-        
+            
         await page.screenshot({ path: `./screenshots/${name}.png`});
         await browser.close();
-
+    
         const attachment = new MessageAttachment(`./screenshots/${name}.png`);
-        
+            
         const embed = new MessageEmbed()
             .setTitle(args[0])
             .setURL(url)
             .setImage(`attachment://${name}.png`)
             .setFooter(`${message.author.tag}`, message.author.displayAvatarURL())
             .setTimestamp();
-
+    
         await message.channel.send({ embed, files: [attachment] });
-
-        fs.unlinkSync(`./screenshots/${name}.png`);
+    
+        fs.unlinkSync(`./screenshots/${name}.png`);    
     }
 }
