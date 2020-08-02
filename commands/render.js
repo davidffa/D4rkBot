ca0b4143f8721517a3bc8a40a51d734555186843
David <davidfilipeamorim@gmail.com>
2020-08-02 16:39:26 +0100

    Wiki command and render fixes

--- commands/render.js
+++ commands/render.js
@@ -1,6 +1,7 @@
 const { MessageEmbed, MessageAttachment } = require('discord.js');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const sightengine = require('sightengine')('1189037133', 'a5h33PCujWJRJbdBkRi2');
 
 module.exports = {
     name: 'render',
@@ -9,12 +10,14 @@ module.exports = {
     category: 'Outros',
     usage: '<URL>',
     cooldown: 3,
+    guildOnly: true,
     async execute(client, message, args, prefix) {
         if (!args.length) 
             return message.channel.send(`:x: Argumentos em falta, **Usa:** ${prefix}render <URL>`);
 
         if (message.author.id != '334054158879686657')
-            return message.channel.send(':x: Em manutenção!');
+            return;
+
         const name = 'screenshot' + Math.floor((Math.random() * 100) + 1);
         let url;
 
@@ -24,7 +27,7 @@ module.exports = {
             url = args[0];
 
         if (!fs.existsSync('./screenshots')) 
-            fs.mkdirSync('./screenshots')
+            fs.mkdirSync()
 
         const browser = await puppeteer.launch({
             args: [
@@ -48,18 +51,47 @@ module.exports = {
             
         await page.screenshot({ path: `./screenshots/${name}.png`});
         await browser.close();
-    
-        const attachment = new MessageAttachment(`./screenshots/${name}.png`);
+
+        if (message.channel.nsfw) {
+            const attachment = new MessageAttachment(`./screenshots/${name}.png`);
             
-        const embed = new MessageEmbed()
+            const embed = new MessageEmbed()
             .setTitle(args[0])
             .setURL(url)
             .setImage(`attachment://${name}.png`)
             .setFooter(`${message.author.tag}`, message.author.displayAvatarURL())
             .setTimestamp();
     
-        await message.channel.send({ embed, files: [attachment] });
+            await message.channel.send({ embed, files: [attachment] });
+
+            return fs.unlinkSync(`./screenshots/${name}.png`); 
+        }
+
+        sightengine.check(['nudity']).set_file(`./screenshots/${name}.png`).then(async result => {
+            console.log(result)
+            if (result.status !== 'success') 
+                return message.channel.send(':x: Erro ao verificar a imagem!');
+            
+            if (result.nudity.raw > 0.01)  {
+                fs.unlinkSync(`./screenshots/${name}.png`);  
+                return message.channel.send(':x: Não podes renderizar conteúdo pornográfico!');
+            }
+
+            const attachment = new MessageAttachment(`./screenshots/${name}.png`);
+            
+            const embed = new MessageEmbed()
+            .setTitle(args[0])
+            .setURL(url)
+            .setImage(`attachment://${name}.png`)
+            .setFooter(`${message.author.tag}`, message.author.displayAvatarURL())
+            .setTimestamp();
     
-        fs.unlinkSync(`./screenshots/${name}.png`);    
+            await message.channel.send({ embed, files: [attachment] });
+
+            fs.unlinkSync(`./screenshots/${name}.png`);  
+        }).catch(function(err) {
+            message.channel.send(':x: Erro ao verificar a imagem!');
+            console.log(err);
+        }); 
     }
 }
---
+++ commands/wiki.js
@@ -0,0 +1,80 @@
+const { MessageEmbed } = require('discord.js');
+const algorithmia = require('algorithmia');
+const sentenceBoundaryDetection = require('sbd');
+
+module.exports = {
+    name: 'wiki',
+    description: 'Procura algo na wikipedia',
+    aliases: ['wikipedia'], 
+    category: 'Outros',
+    usage: '<Palavra/Frase>',
+    cooldown: 3,
+    async execute(client, message, args, prefix) {
+        if (!args.length) 
+            return message.channel.send(`:x: Argumentos em falta, **Usa:** ${prefix}wiki <Palavra/Frase>`);
+
+        let content = args.join(' ');
+        const input = {
+            'articleName': content,
+            'lang': 'pt'
+        };
+
+        const msg = await message.channel.send('<a:lab_loading:643912893011853332> A procurar...');
+
+        await fetchContentFromWikipedia();
+        if (content == args.join(' ')) return;
+        sanitizeContent();
+        breakContentIntoSentences();
+        limitMaximumSentences();
+        
+        await msg.edit('', new MessageEmbed()
+            .setTitle(`Wikipedia: (${args.join(' ')})`)
+            .setColor('RANDOM')
+            .setDescription(content.join('\n'))
+            .setFooter(`${message.author.tag}`, message.author.displayAvatarURL())
+            .setTimestamp());
+
+        async function fetchContentFromWikipedia() {   
+            const algorithmiaAuthenticated = algorithmia(process.env.AlgorithmiaKey);
+            const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2?timeout=300');
+            const wikipediaResponse = await wikipediaAlgorithm.pipe(input);
+            try {
+                const wikipediaContent = wikipediaResponse.get();
+                content = wikipediaContent.content;
+            }catch(err) {
+                return await msg.edit(`:x: Não encontrei nada na wikipédia para \`${args.join(' ')}\``)
+            }
+        }
+
+        function sanitizeContent() {
+            const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown();
+            const withoutDatesInParenteses = removeDatesInParentheses(withoutBlankLinesAndMarkdown);
+            content = withoutDatesInParenteses;
+
+            function removeBlankLinesAndMarkdown() {
+                const allLines = content.split('\n');
+
+                const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                    if (line.trim().length === 0 || line.trim().startsWith('=')) {
+                        return false;
+                    }
+                    return true;
+                });
+
+                return withoutBlankLinesAndMarkdown.join(' ');
+            }
+
+            function removeDatesInParentheses(text) {
+                return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ');
+            }
+        }
+
+        function breakContentIntoSentences() {
+            content = sentenceBoundaryDetection.sentences(content);
+        }
+
+        function limitMaximumSentences() {
+            content = content.slice(0, 5)
+        }
+    }
+}
