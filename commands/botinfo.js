768e6cdc01543d362384692d9cffa4abb0037a36
David <davidfilipeamorim@gmail.com>
2020-08-20 16:37:27 +0100

    Package updates

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -27,7 +27,7 @@ module.exports = {
             .addField(':ping_pong: Ping da API', `\`${Math.round(client.ws.ping)}ms\``, true)
             .addField('<:bot_badgehypesquad:590943982436089858> Prefixos', `Padrão: \`.\`\n No servidor: \`${prefix}\``, true)
             .addField('<:lang_js:427101545478488076> Versão NodeJS', '`v12.18.3`', true)
-            .addField('<a:lab_blobdiscord:643917538555854849> Versão do Discord.js', '`v12.2.0`', true)
+            .addField('<a:lab_blobdiscord:643917538555854849> Versão do Discord.js', '`v12.3.1`', true)
             .addField(':bank: Banco de dados', '`MongoDB`', true)
             .addField('<a:carregando:488783607352131585> CPU', `\`${cpuUsage}\` %`, true)
             .addField('RAM', `\`${(process.memoryUsage().rss/1024/1024).toFixed(0)}MB\``, true)
