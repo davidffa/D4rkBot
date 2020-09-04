c68c0d5a65785285f04107d45dfe8a52cd10d2c9
David <davidfilipeamorim@gmail.com>
2020-09-04 17:35:28 +0100

    Add Reaction to close eval result

--- commands/eval.js
+++ commands/eval.js
@@ -8,7 +8,7 @@ module.exports = {
     usage: '<código>',
     category: 'Desenvolvedor',
     cooldown: 1,
-    execute(client, message, args) {
+    async execute(client, message, args) {
         if (message.author.id !== '334054158879686657') {
             return message.reply(':x: Não tens permissão!');
         }
@@ -37,7 +37,21 @@ module.exports = {
             )
                 .setTimestamp()
                 .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
-            message.channel.send(embed);
+            const msg = await message.channel.send(embed);
+            await msg.react('751062867444498432');
+
+            const filter = (r, u) => r.me && (u.id === message.author.id);
+            const collector = msg.createReactionCollector(filter, { max: 1, time: 60 * 1000 });
+
+            collector.on('collect', async r => {
+                switch(r.emoji.name) {
+                    case 'x_':
+                        if (message.guild.me.hasPermission('MANAGE_MESSAGES'))
+                            msg.reactions.removeAll();
+                        msg.edit('<a:lab_verificado:643912897218740224> Eval fechada.', { embed: null });  
+                        break;
+                }
+            });
         } catch (err) {
             message.channel.send(`:x: ERRO: ${err.message}`);
         }
