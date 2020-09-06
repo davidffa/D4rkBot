8aad3f58c48c47119d219c4dac25ed67081a2904
David <davidfilipeamorim@gmail.com>
2020-09-06 15:45:03 +0100

    Converted queue cmd to LavaLink

--- commands/queue.js
+++ commands/queue.js
@@ -1,18 +1,15 @@
-const play = require('./play');
-
 module.exports = {
     name: 'queue',
-    description: 'Mostra a lista de músicas.',
-    aliases: ['lista', 'list', 'musiclist'], 
+    description: 'Vê as músicas que estão na queue',
     category: 'Musica',
     guildOnly: true,
     cooldown: 5,
     execute(client, message) {
-        try {
-            play.play.queue(message, message.author);
-        }catch (e) {
-            message.channel.send(':x: Não estou a tocar nada de momento!');
-            console.log(e.message);
-        }
+        const player = client.music.players.get(message.guild.id);
+
+        if (!player)
+            return message.channel.send(':x: Não estou a tocar nada de momento!');
+
+        message.channel.send(`:bookmark_tabs: Lista de músicas: \`${player.queue.map(queueItem => queueItem.title)}\``, { split: true });
     }
 }
