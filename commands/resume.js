8a89b082d2d37319090ca965cc81d793d6df3a39
David <davidfilipeamorim@gmail.com>
2020-09-06 15:45:12 +0100

    Converted resume cmd to LavaLink

--- commands/resume.js
+++ commands/resume.js
@@ -1,5 +1,3 @@
-const play = require('./play');
-
 module.exports = {
     name: 'resume',
     description: 'Retoma a música atual.',
@@ -8,11 +6,21 @@ module.exports = {
     guildOnly: true,
     cooldown: 2,
     execute(client, message) {
-        try {
-            play.play.resume(message, message.author);
-        }catch (e) {
-            message.channel.send(':x: Não estou a tocar nada de momento!');
-            console.log(e.message);
-        }
+        const player = client.music.players.get(message.guild.id);
+
+        if (!player)
+            return message.channel.send(':x: Não estou a tocar nada de momento!');
+        
+        const voiceChannel = message.member.voice.channel;
+
+        if (voiceChannel && voiceChannel.id && voiceChannel.id !== player.voiceChannel.id)
+            return message.channel.send(':x: Precisas de estar no meu canal de voz para usar esse comando!');
+
+        if (player.playing)
+            return message.channel.send(':x: A música já está a tocar!');
+
+        player.pause(player.playing);
+
+        message.channel.send(':play_pause: Música retomada!');
     }
 }
