95dea1bff232b81b0f8ad707d87fed8b247526f4
David <davidfilipeamorim@gmail.com>
2020-09-06 15:44:39 +0100

    Converted np cmd to LavaLink

--- commands/np.js
+++ commands/np.js
@@ -1,4 +1,5 @@
-const play = require('./play');
+const { MessageEmbed } = require('discord.js');
+const mstohour = require('../utils/mstohour');
 
 module.exports = {
     name: 'np',
@@ -8,11 +9,18 @@ module.exports = {
     guildOnly: true,
     cooldown: 5,
     execute(client, message) {
-        try {
-            play.play.np(message);
-        }catch (e) {
-            message.channel.send(':x: Não estou a tocar nada de momento!');
-            console.log(e.message);
-        }
+        const player = client.music.players.get(message.guild.id);
+
+        if (!player || !player.queue[0])
+            return message.channel.send(':x: Não estou a tocar nada de momento!');
+
+        const embed = new MessageEmbed()
+                    .setTitle('<a:Labfm:482171966833426432> A Tocar')
+                    .setColor('RANDOM')
+                    .setDescription(`\`${player.queue[0].title}\` requisitado por \`${player.queue[0].requester.username}#${player.queue[0].requester.discriminator}\` com a duração de \`${mstohour(player.position)}/${mstohour(player.queue[0].duration)}\``)
+                    .setURL(player.queue[0].uri)
+                    .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+                    .setTimestamp();
+        message.channel.send(embed);
     }
 }
