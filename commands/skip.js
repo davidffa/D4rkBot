a43085a9b7555509bc076507722cf6593d05d8b5
David <davidfilipeamorim@gmail.com>
2020-09-06 15:44:54 +0100

    Converted skip cmd to LavaLink

--- commands/skip.js
+++ commands/skip.js
@@ -1,18 +1,23 @@
-const play = require('./play');
-
 module.exports = {
     name: 'skip',
     description: 'Pula a música atual.',
     aliases: ['s', 'pular'], 
     category: 'Musica',
     guildOnly: true,
-    cooldown: 4,
+    cooldown: 2,
     execute(client, message) {
-        try {
-            play.play.skip(message, message.author);
-        }catch (e) {
-            message.channel.send(':x: Não estou a tocar nada de momento!');
-            console.log(e.message);
-        }
+        const player = client.music.players.get(message.guild.id);
+
+        if (!player)
+            return message.channel.send(':x: Não estou a tocar nada de momento!');
+        
+        const voiceChannel = message.member.voice.channel;
+
+        if (voiceChannel && voiceChannel.id && voiceChannel.id !== player.voiceChannel.id)
+            return message.channel.send(':x: Precisas de estar no meu canal de voz para usar esse comando!');
+
+        player.stop();
+
+        message.channel.send(':fast_forward: Música pulada!');
     }
 }
