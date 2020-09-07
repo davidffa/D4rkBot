bcf310357ccdca2420911971986d554a4b355774
David <davidfilipeamorim@gmail.com>
2020-09-07 18:53:52 +0100

    Pause music on voiceState event

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -4,8 +4,10 @@ module.exports.run = (client, oldState, newState) => {
 
     if (client.music.players.get(oldState.guild.id) && voiceChannel === client.music.players.get(oldState.guild.id).voiceChannel) {
         if (voiceChannel.members.size === 1) {
+            client.music.players.get(oldState.guild.id).pause(true);
+            client.music.players.get(oldState.guild.id).textChannel.send(':warning: Pausei a música porque fiquei sozinho no canal de voz, se ninguem aparecer irei sair em 5 minutos.');
             const timeout = setTimeout(() => {
-                client.music.players.get(oldState.guild.id).textChannel.send(':warning: Parei de tocar música por inatividade no canal de voz.');
+                client.music.players.get(oldState.guild.id).textChannel.send(':x: Saí do canal de voz porque fiquei sozinho mais de 5 minutos.');
                 client.music.players.destroy(oldState.guild.id);
             }, 5 * 60 * 1000);
             client.voiceStateTimeouts.set(oldState.guild.id, timeout);
@@ -14,6 +16,7 @@ module.exports.run = (client, oldState, newState) => {
 
     if (client.music.players.get(newState.guild.id) && voiceChannel2 === client.music.players.get(newState.guild.id).voiceChannel) {
         if (client.voiceStateTimeouts.has(newState.guild.id)) {
+            client.music.players.get(newState.guild.id).pause(false);
             clearTimeout(client.voiceStateTimeouts.get(newState.guild.id));
             client.voiceStateTimeouts.delete(newState.guild.id);
         }
