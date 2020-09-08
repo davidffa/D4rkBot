669b0453be4669c1206a0daf69b8f017e829bbec
David <davidfilipeamorim@gmail.com>
2020-09-08 12:20:07 +0100

    Chatclear cmd delete msg after 10s

--- commands/chatclear.js
+++ commands/chatclear.js
@@ -9,7 +9,7 @@ module.exports = {
     guildOnly: true,
     args: 1,
     cooldown: 5,
-    execute(client, message, args, prefix) {
+    async execute(client, message, args, prefix) {
         if (!message.member.hasPermission('MANAGE_MESSAGES'))
             return message.channel.send(':x: Não tens permissão!');
 
@@ -17,13 +17,13 @@ module.exports = {
             return message.channel.send(':x: Não tenho permissão para apagar mensagens!');
 
         if (!args.length)
-            return message.channel.send(`**Usa:** ${prefix}chatclear <Nº de mensagens (entre 2 e 99)>`);
+            return message.channel.send(`**Usa:** ${prefix}chatclear <Nº de mensagens (entre 2 e 98)>`);
         
         const number = args[0];
         if (isNaN(number)) 
             return message.channel.send(':x: Número inválido!');
-        if (args[0] < 2 || args[0] >= 100) 
-            return message.channel.send(':x: Só consigo limpar entre 2 e 99 mensagens de cada vez!');
+        if (args[0] < 2 || args[0] >= 99) 
+            return message.channel.send(':x: Só consigo limpar entre 2 e 98 mensagens de cada vez!');
         
         const embed = new MessageEmbed()
             .setColor('RANDOM')
@@ -31,13 +31,15 @@ module.exports = {
             .setTimestamp()
             .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 
-        message.channel.bulkDelete(parseInt(args[0]), { filterOld: true }).then(msgs => {
+        message.channel.bulkDelete(parseInt(args[0])+1, { filterOld: true }).then(msgs => {
             if (parseInt(args[0]) !== msgs.size) 
                 embed.setDescription(`<a:lab_verificado:643912897218740224> Limpas \`${msgs.size}\` mensagens\n
                                       :x: Não consigo apagar mais mensagens! Só consigo apagar mensagens com data de envio menor que 2 semanas.`);
             else
                 embed.setDescription(`<a:lab_verificado:643912897218740224> Limpas \`${msgs.size}\` mensagens`);
-            message.channel.send(embed);  
+            const msg = await message.channel.send(embed);  
+
+            msg.delete({ timeout: 10000 });
         });
     }
 }
