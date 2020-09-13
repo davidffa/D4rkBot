4e041434a189408ae84b855a45420d673a5fb204
David <davidfilipeamorim@gmail.com>
2020-09-13 15:26:25 +0100

    Search user by tag

--- commands/unban.js
+++ commands/unban.js
@@ -1,25 +1,26 @@
 module.exports = {
     name: 'unban',
-    description: 'Tira o ban a alguém', 
+    description: 'Tira o ban a alguém',
     usage: '<user>',
     category: 'Admin',
     guildOnly: true,
     cooldown: 3,
     args: 1,
-    async execute(client, message, args, prefix) { 
+    async execute(client, message, args, prefix) {
         if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply(':x: Não tens permissão para desbanir membros!');
         if (!message.guild.member(client.user.id).hasPermission('BAN_MEMBERS')) return message.channel.send(':x: Não tenho permissão para desbanir membros!');
 
         const bans = await message.guild.fetchBans();
-        bans.map(user => {
-            if (user.user.username.toLowerCase().startsWith(args[0].toLowerCase())) {
-                message.guild.members.unban(user.user.id).then(usr => {
-                    message.channel.send(`<a:lab_verificado:643912897218740224> Desbanis-te o \`${usr.username}\`.`)
-                }).catch(err => {
-                    message.channel.send(':x: Ocorreu um erro!');
-                    console.error(err.message);
-                });
-            }
+        const user = bans.filter(user => user.user.tag.toLowerCase().startsWith(args[0].toLowerCase())).first();
+        
+        if (!user)
+            return message.channel.send(':x: Membro não encontrado!');
+
+        message.guild.members.unban(user.user.id).then(usr => {
+            message.channel.send(`<a:lab_verificado:643912897218740224> Desbanis-te o \`${usr.tag}\`.`)
+        }).catch(err => {
+            message.channel.send(':x: Ocorreu um erro!');
+            console.error(err.message);
         });
     }
 };
