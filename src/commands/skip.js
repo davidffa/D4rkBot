cbcf8782a08cecfdb1e92c5837e2b04cb60f4125
David <davidfilipeamorim@gmail.com>
2020-09-14 20:19:56 +0100

    Only admin or requester can skip the song

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -16,8 +16,14 @@ module.exports = {
         if (!voiceChannel || (voiceChannel && voiceChannel.id !== player.voiceChannel.id))
             return message.channel.send(':x: Precisas de estar no meu canal de voz para usar esse comando!');
 
-        player.stop();
-
-        message.channel.send(':fast_forward: Música pulada!');
+        if (message.author.id === player.queue[0].requester.id) {
+            player.stop();
+            message.channel.send(':fast_forward: Música pulada!');
+        }else if (message.member.permissions.has('ADMINISTRATOR')) {
+            player.stop();
+            message.channel.send(':fast_forward: Música pulada por um `ADMIN`!');
+        }else {
+            message.channel.send(':x: Apenas quem requisitou esta música ou um admin a pode pular!');
+        }
     }
 }
