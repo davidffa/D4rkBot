becaee14eea0edf94adee3c3abec77203b6960c9
David <davidfilipeamorim@gmail.com>
2020-09-16 14:44:20 +0100

    Banlist cmd optimization

--- src/commands/banlist.js
+++ src/commands/banlist.js
@@ -6,19 +6,18 @@ module.exports = {
     cooldown: 3,
     async execute(client, message) { 
         if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply(':x: Não tens permissão para ver a lista de membros banidos!');
-        if (!message.guild.member(client.user.id).hasPermission('BAN_MEMBERS')) return message.channel.send(':x: Não tenho permissão para ver a lista de membros banidos!');
+        if (!message.guild.me.hasPermission('BAN_MEMBERS')) return message.channel.send(':x: Não tenho permissão para ver a lista de membros banidos!');
 
         let msg = '';
 
         const bans = await message.guild.fetchBans();
+
+        if (!bans.first())
+            return message.channel.send(':x: Este servidor não tem membros banidos!');
         bans.map(user => {
             msg += `\`${user.user.tag}\`, `;
         });
 
-        if (msg === '') {
-            message.channel.send('Este servidor não tem membros banidos!');
-        }else {
-            message.channel.send(':bookmark_tabs: Lista de membros banidos:\n' + msg, { split: true });
-        }
+        message.channel.send(':bookmark_tabs: Lista de membros banidos:\n' + msg, { split: true });
     }
 };
