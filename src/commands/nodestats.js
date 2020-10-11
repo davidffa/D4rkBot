17f8082f4c2c69f6ca066c7008e2361b306057c7
David <davidfilipeamorim@gmail.com>
2020-10-11 14:06:06 +0100

    Change playingPlayers to players

--- src/commands/nodestats.js
+++ src/commands/nodestats.js
@@ -13,7 +13,7 @@ module.exports = {
             .setColor('RANDOM')
             .setTitle('Status do Node do LavaLink')
             .addField(':id: Nome', `\`${node.options.identifier}\``, true)
-            .addField(':calendar: Players a tocar', `\`${node.stats.playingPlayers}\``, true)
+            .addField(':calendar: Players a tocar', `\`${node.stats.players}\``, true)
             .addField('<a:malakoi:478003266815262730> Uptime', `\`${msToDate(node.stats.uptime)}\``, true)
             .addField('<a:carregando:488783607352131585> CPU', `Cores: \`${node.stats.cpu.cores}\`\nLavalink: \`${node.stats.cpu.lavalinkLoad.toFixed(2)}%\`\nSistema: \`${node.stats.cpu.systemLoad.toFixed(2)}%\``, true)
             .addField('<:ram:751468688686841986> RAM', `\`${(node.stats.memory.used / 1024 / 1024).toFixed(0)}\`MB`, true)
