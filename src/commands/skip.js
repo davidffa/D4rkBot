225d8a371fd401d8ffd31d4af9612e3eb367974f
David <davidfilipeamorim@gmail.com>
2020-10-26 19:27:44 +0000

    Music commands improvements

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -16,20 +16,19 @@ module.exports = {
         if (!voiceChannel || (voiceChannel && voiceChannel.id !== player.voiceChannel))
             return message.channel.send(':x: Precisas de estar no meu canal de voz para usar esse comando!');
 
-        if (message.author.id === player.queue.current.requester.id) {
+        function stopMusic() { 
             player.stop();
             if (!player.queue[0]) {
                 player.destroy();
                 return message.channel.send(':bookmark_tabs: A lista de músicas acabou!');
             }
             message.channel.send(':fast_forward: Música pulada!');
-        }else if (message.guild.channels.cache.get(player.voiceChannel).permissionsFor(message.author).has('MOVE_MEMBERS')) {
-            player.stop();
-            if (!player.queue[0]) {
-                player.destroy();
-                return message.channel.send(':bookmark_tabs: A lista de músicas acabou!');
-            }
-            message.channel.send(':fast_forward: Música pulada por um `ADMIN`!');
+        }
+
+        if (message.author.id === player.queue.current.requester.id 
+            || message.guild.channels.cache.get(player.voiceChannel).permissionsFor(message.author).has('MOVE_MEMBERS')
+            || voiceChannel.size <= 3) {
+            stopMusic();
         }else {
             message.channel.send(':x: Apenas quem requisitou esta música ou alguém com a permissão `Mover Membros` a pode pular!');
         }
--- src/commands/stop.js
+++ src/commands/stop.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'stop',
     description: 'Para a música atual, sai do canal de voz e limpa a lista de músicas.',
-    aliases: ['parar', 'disconnect', 'desconectar', 'leave', 'sair', 'quit', 'q'], 
+    aliases: ['parar', 'disconnect', 'desconectar', 'leave', 'sair', 'quit', 'q'],
     category: 'Musica',
     guildOnly: true,
     cooldown: 5,
@@ -10,12 +10,12 @@ module.exports = {
 
         if (!player)
             return message.channel.send(':x: Não estou a tocar nada de momento!');
-        
-        if (!message.guild.channels.cache.get(player.voiceChannel).permissionsFor(message.author).has('MOVE_MEMBERS'))
-            return message.channel.send(':x: Precisas da permissão `Mover Membros` para usar este comando!');
 
-        player.destroy();
+        if (message.guild.channels.cache.get(player.voiceChannel).members.size === 1 || (message.member.voice.channel && message.member.voice.channel.id === player.voiceChannel && message.member.voice.channel.members.size <= 3)) {
+            player.destroy();
 
-        message.channel.send('<a:lab_verificado:643912897218740224> Parei de tocar música e saí do canal de voz!');
+            return message.channel.send('<a:lab_verificado:643912897218740224> Parei de tocar música e saí do canal de voz!');
+        }
+        message.channel.send(':x: Precisas da permissão `Mover Membros` para usar este comando!');
     }
 }
