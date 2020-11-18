184ad8d15a1724f4086ace4e8e38a871f82686d4
David <davidfilipeamorim@gmail.com>
2020-11-18 20:39:22 +0000

    Userinfo cmd improvements

--- src/commands/userinfo.js
+++ src/commands/userinfo.js
@@ -1,6 +1,5 @@
 const { MessageEmbed } = require('discord.js');
 const moment = require('moment');
-const { isNumber } = require('util');
 moment.locale('pt-PT');
 
 function getStatus(status) {
@@ -10,53 +9,78 @@ function getStatus(status) {
     return outStatus;
 }
 
+function getDevice(user) {
+    if (!user.presence.clientStatus) return null;
+    const devices = Object.keys(user.presence.clientStatus);
+
+    const deviceList = devices.map(device => {
+        if (device === 'desktop')
+            return ':computer:';
+        else if (device === 'mobile')
+            return ':mobile_phone:';
+        else return ':globe_with_meridians:';
+    });
+
+    return deviceList.join(' - ');
+}
+
 module.exports = {
     name: 'userinfo',
-    description: 'Informações sobre o servidor', 
+    description: 'Informações sobre algum utilizador',
     usage: '[nome]',
+    aliases: ['ui', 'usrinfo'],
     category: 'Info',
     guildOnly: true,
     cooldown: 5,
-    execute(client, message, args) {
-        let user;
+    async execute(client, message, args) {
+        let user = message.mentions.users.first();
 
         if (!args.length) {
             user = message.author;
-        }else {
-            const userMentioned = message.mentions.users.first();
-            if (userMentioned) {
-                user = userMentioned;
-            }else if (Number(args[0]) && args[0].length === 18) {
-                user = client.users.cache.get(args[0]);
-            }else {
-                message.guild.members.cache.map(member => {
-                    if (member.displayName === args.join(' ')) 
+        } else {
+            if (Number(args[0])) {
+                user = await client.users.fetch(args[0]);
+            }
+
+            if (!user) {
+                message.guild.members.cache.forEach(member => {
+                    if (member.displayName === args.join(' '))
                         user = member.user;
                 });
+            }
 
-                if (!user) {
-                    message.guild.members.cache.map(member => {
-                        if (member.displayName.toLowerCase().startsWith(args.join(' ').toLowerCase())) {
-                            user = member.user;
-                        }
-                    });
-                }
+            if (!user) {
+                message.guild.members.cache.forEach(member => {
+                    if (member.displayName.toLowerCase().startsWith(args.join(' ').toLowerCase())) {
+                        user = member.user;
+                    }
+                });
             }
         }
 
         if (!user) return message.channel.send(':x: Utilizador não encontrado!');
-        
+
         const embed = new MessageEmbed()
             .setColor('RANDOM')
-            .setTitle(':information_source: Informações de Utilizador')
+            .setTitle(`Informações de ${user.bot ? '<:bot_bot:568569868358516770> ' : ''}${user.username}`)
             .addField(':bookmark_tabs: Tag', `\`${user.tag}\``, true)
             .addField(':closed_book: ID', `\`${user.id}\``, true)
             .addField(':calendar: Conta criada em', `\`${moment(user.createdAt).format('L')} (${moment(user.createdAt).startOf('day').fromNow()})\``, true)
-            .addField(':calendar: Entrada no servidor', `\`${moment(message.guild.member(user).joinedAt).format('L')} (${moment(message.guild.member(user).joinedAt).startOf('day').fromNow()})\``, true)
-            .addField(':shrug: Status', `\`${getStatus(user.presence.status)}\``, true)
             .setTimestamp()
             .setThumbnail(user.displayAvatarURL())
             .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
+
+        if (message.guild.members.cache.get(user.id)) {
+            embed.addField(':calendar: Entrada no servidor', `\`${moment(message.guild.member(user).joinedAt).format('L')} (${moment(message.guild.member(user).joinedAt).startOf('day').fromNow()})\``, true)
+                .addField(':shrug: Status', `\`${getStatus(user.presence.status)}\``, true);
+
+            const device = getDevice(user);
+
+            if (device) {
+                embed.addField('Dispositivos :technologist:', device, true);
+            }
+        }
+
         return message.channel.send(embed);
     }
 }
