f7e099c88fa1c239a838088bdb8c248176ede007
David <davidfilipeamorim@gmail.com>
2020-11-25 18:22:41 +0000

    Autorole cmd improvements

--- src/commands/autorole.js
+++ src/commands/autorole.js
@@ -8,9 +8,11 @@ module.exports = {
     guildOnly: true,
     cooldown: 5,
     async execute(client, message, args, prefix) {
-        if (!message.member.hasPermission('MANAGE_ROLES') && message.author.id != '334054158879686657')
-            return message.channel.send(':x: Não tens permissão!');
+        if (!message.member.hasPermission('MANAGE_ROLES') && message.author.id !== '334054158879686657')
+            return message.channel.send(':x: Precisas da permissão `MANAGE_ROLES` para usar este comando!');
+
         const guild = await guildDB.findOne({ guildID: message.guild.id });
+
         if (!args.length) {
             if (guild && guild.roleID) {
                 const role = message.guild.roles.cache.get(guild.roleID);
@@ -23,6 +25,7 @@ module.exports = {
             }
             return message.channel.send(`**Use:** ${prefix}autorole <Nome do cargo> (${prefix}autorole 0 - para desligar esta função)`);
         }
+        
         if (args[0] === '0') {
             if (guild && guild.roleID) {
                 await guildDB.updateOne({ guildID: message.guild.id }, {
@@ -38,46 +41,28 @@ module.exports = {
             message.channel.send(":warning: Não tenho permissão para alterar cargos de membros.");
         }
 
-        const role = message.mentions.roles.first();
+        const role = message.mentions.roles.first() ||
+            message.guild.roles.cache.get(args[0]) ||
+            message.guild.roles.cache.filter(role => role.name.toLowerCase() === args[0].toLowerCase()).first();
 
-        if (role) {
-            if (guild) {
-                const roleID = guild.roleID;
-                await guildDB.updateOne({ guildID: message.guild.id }, {
-                    roleID: role.id,
-                }).catch(console.log)
-                if (roleID)
-                    message.channel.send(`<a:lab_verificado:643912897218740224> Autorole alterado para \`${role.name}\``);
-                else
-                    message.channel.send(`<a:lab_verificado:643912897218740224> Cargo \`${role.name}\` setado como autorole.`);
-            } else {
-                await guildDB.create({
-                    guildID: message.guild.id,
-                    roleID: role.id,
-                });
-                message.channel.send(`<a:lab_verificado:643912897218740224> Cargo \`${role.name}\` setado como autorole.`)
-            }
+        if (!role)
+            return message.channel.send(`:x: Cargo \`${args[0]}\` não encontrado!`);
+
+        if (guild) {
+            const roleID = guild.roleID;
+            await guildDB.updateOne({ guildID: message.guild.id }, {
+                roleID: role.id,
+            }).catch(console.log)
+            if (roleID)
+                message.channel.send(`<a:lab_verificado:643912897218740224> Autorole alterado para \`${role.name}\``);
+            else
+                message.channel.send(`<a:lab_verificado:643912897218740224> Cargo \`${role.name}\` setado como autorole.`);
         } else {
-            const role = message.guild.roles.cache.filter(role => role.name.toLowerCase() === args[0].toLowerCase()).first();
-
-            if (!role)
-                return message.channel.send(`:x: Cargo \`${args[0]}\` não encontrado!`);
-            if (guild) {
-                const roleID = guild.roleID;
-                await guildDB.updateOne({ guildID: message.guild.id }, {
-                    roleID: role.id,
-                }).catch(console.log)
-                if (roleID)
-                    message.channel.send(`<a:lab_verificado:643912897218740224> Autorole alterado para \`${role.name}\``);
-                else
-                    message.channel.send(`<a:lab_verificado:643912897218740224> Cargo \`${role.name}\` setado como autorole.`);
-            } else {
-                await guildDB.create({
-                    guildID: message.guild.id,
-                    roleID: role.id,
-                });
-                message.channel.send(`<a:lab_verificado:643912897218740224> Cargo \`${role.name}\` setado como autorole.`)
-            }
+            await guildDB.create({
+                guildID: message.guild.id,
+                roleID: role.id,
+            });
+            message.channel.send(`<a:lab_verificado:643912897218740224> Cargo \`${role.name}\` setado como autorole.`)
         }
     }
 }
