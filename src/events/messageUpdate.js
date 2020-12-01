80d8b400461253877c18f6573df0def22f57f932
David <davidfilipeamorim@gmail.com>
2020-12-01 18:50:20 +0000

    Set max edits to 3

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -1,4 +1,4 @@
 module.exports.run = (client, oldMessage, newMessage) => {
-    if (oldMessage.content === newMessage.content) return;
+    if (oldMessage.content === newMessage.content || oldMessage.edits.length > 3) return;
     client.emit('message', newMessage);
 }
