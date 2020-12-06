a04268b1bfd58bba795dcc4e1141b755655ae6fe
David <davidfilipeamorim@gmail.com>
2020-12-06 16:04:49 +0000

    Add twitter field and bug fix

--- src/commands/github.js
+++ src/commands/github.js
@@ -20,18 +20,19 @@ module.exports = {
         const embed = new MessageEmbed()
             .setTitle(`<:github:784791056670654465> Perfil do ${user.login}`)
             .setColor('RANDOM')
-            .addField(':bust_in_silhouette: Nome', user.name, true)
+            .addField(':bust_in_silhouette: Nome', user.name || user.login, true)
             .addField(':id: ID', user.id, true)
             .addField(':bookmark: Repositórios Públicos', user.public_repos, true)
             .addField('<:followers:784795303156908032> Seguidores', user.followers, true)
             .addField(':busts_in_silhouette: A seguir', user.following, true)
-            .setThumbnail(user.avatar_url)
+            .setThumbnail(`${user.avatar_url}${Math.floor(Math.random() * 10000)}`)
             .setURL(user.html_url)
             .setTimestamp()
             .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true }));
 
         user.email && embed.addField(':e_mail: Email', user.email, true);
         user.company && embed.addField(':classical_building: Empresa', user.company, true);
+        user.twitter_username && embed.addField('<:twitter:785165170547753002> Twitter', `[@${user.twitter_username}](https://twitter.com/${user.twitter_username})`, true);
         user.location && embed.addField(':map: Localização', user.location, true);
 
         embed.addField(':calendar: Criado em', `${moment(user.created_at).format('L')} (${moment(user.created_at).startOf('day').fromNow()})`, true);
