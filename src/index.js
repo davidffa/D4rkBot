de634e92b7400971a4efd166131b1f9bcb149ea8
David <davidfilipeamorim@gmail.com>
2020-12-27 18:04:22 +0000

    Set ready event to .once

--- src/index.js
+++ src/index.js
@@ -30,6 +30,10 @@ const eventFiles = readdirSync('./src/events').filter(file => file.endsWith('.js
 for (const file of eventFiles) {
     const event = require(`./events/${file}`);
     const eventName = file.split('.')[0];
+    if (eventName === 'ready') {
+        client.once(eventName, (...args) => event.run(client, ...args));
+        continue;
+    }
     client.on(eventName, (...args) => event.run(client, ...args));
 }
 
