bb18c98a50ae6bbc530b6dcfc6ae9a662521e0d9
David <davidfilipeamorim@gmail.com>
2021-03-01 18:33:00 +0000

    Replace Math.floor to ~~

--- src/utils/msToDate.ts
+++ src/utils/msToDate.ts
@@ -1,9 +1,9 @@
 export default (time: number): string => {
     time = Math.round(time / 1000)
     const s = time % 60,
-    m = Math.floor((time / 60) % 60),
-    h = Math.floor((time / 60 / 60) % 24),
-    d = Math.floor(time / 60 / 60 / 24);
+    m = ~~((time / 60) % 60),
+    h = ~~((time / 60 / 60) % 24),
+    d = ~~(time / 60 / 60 / 24);
 
     return `${d}D:${h}H:${m}M:${s}S`;
 }
--- src/utils/msToHour.ts
+++ src/utils/msToHour.ts
@@ -1,8 +1,8 @@
 export default (time: number): string => {
     time = Math.round(time / 1000)
     const s = time % 60,
-    m = Math.floor((time / 60) % 60),
-    h = Math.floor(time / 60 / 60);
+    m = ~~((time / 60) % 60),
+    h = ~~(time / 60 / 60);
 
     return h
         ? `${String(h).length === 2 ? h : `0${h}`}:${String(m).length === 2 ? m : `0${m}`}:${String(s).length === 2 ? s : `0${s}`}`
