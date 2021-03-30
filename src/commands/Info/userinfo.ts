5805ea5b6b1f142d6c438b104385f6a3d208d488
David Amorim <davidfilipeamorim@gmail.com>
2021-03-30 12:16:43 +0100

    Bug fix

--- src/commands/Info/userinfo.ts
+++ src/commands/Info/userinfo.ts
@@ -86,14 +86,14 @@ export default class Userinfo extends Command {
             DISCORD_EMPLOYEE: '<:staffbadge:803667272186462258>',
             PARTNERED_SERVER_OWNER: '<:partnerbadge:803667091429130260>',
             HYPESQUAD_EVENTS: '<:badgehypesquadevents:803665575703478323>',
-            BUGHUNTER_LEVEL_1: '<:badgebughunter:803664937016360991>',
+            BUG_HUNTER_LEVEL_1: '<:badgebughunter:803664937016360991>',
             HOUSE_BRAVERY: '<:badgehypebravery:803665178720731137>',
             HOUSE_BRILLIANCE: '<:badgehypebrilliance:803665185558102017>',
             HOUSE_BALANCE: '<:badgehypebalance:803665192310800395>',
             EARLY_SUPPORTER: '<:badgeearlysupporter:803665859406725121>',
             TEAM_USER: '',
             SYSTEM: '',
-            BUGHUNTER_LEVEL_2: '<:BugHunterLvl2:803665318274400256>',
+            BUG_HUNTER_LEVEL_2: '<:BugHunterLvl2:803665318274400256>',
             VERIFIED_BOT: '<:vBot1:804393321862397952><:vBot2:804393321854140440>',
             VERIFIED_BOT_DEVELOPER: '<:dev_badge:803665036769230899>'  
         }
