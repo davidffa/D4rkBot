c451fb582a3dc12ce1813cae009bb9412c34bd4c
David Amorim <davidfilipeamorim@gmail.com>
2021-06-03 13:43:23 +0100

    Add invite with slash commands scope

--- src/commands/Info/invite.ts
+++ src/commands/Info/invite.ts
@@ -17,17 +17,19 @@ export default class Invite extends Command {
   execute(ctx: CommandContext): void {
     const embedRes = [
       '<a:blobdance:804026401849475094> **Adicione-me ao seu servidor usando um dos convites abaixo**\n',
-      '[Com permissão de administrador](https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot&permissions=8)',
-      '[Com todas as permissões necessárias](https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot&permissions=1345711190)',
-      '[Sem permissões](https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot&permissions=0)\n\n',
+      '[Com permissão de administrador](https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot+applications.commands&permissions=8)',
+      '[Com todas as permissões necessárias](https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot+applications.commands&permissions=1345711190)',
+      '[Sem permissões](https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot+applications.commands&permissions=0)',
+      '[Sem slash commands](https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot&permissions=0)\n',
       '[Servidor de Suporte](https://discord.gg/dBQnxVCTEw)'
     ];
 
     const res = [
       '<a:blobdance:804026401849475094> **Adicione-me ao seu servidor usando um dos convites abaixo**\n',
-      'Com permissão de administrador -> https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot&permissions=8',
-      'Com todas as permissões necessárias -> https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot&permissions=1345711190',
-      'Sem permissões -> https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot&permissions=0\n\n',
+      'Com permissão de administrador -> https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot+applications.commands&permissions=8',
+      'Com todas as permissões necessárias -> https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot+applications.commands&permissions=1345711190',
+      'Sem permissões -> https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot+applications.commands&permissions=0',
+      'Sem slash commands -> https://discord.com/oauth2/authorize?client_id=499901597762060288&scope=bot&permissions=0\n\n',
       'Servidor de Suporte -> https://discord.gg/dBQnxVCTEw'
     ]
 
