58472f34b5b2a89698a9bcd8452e57d85cb0659f
David Amorim <davidfilipeamorim@gmail.com>
2021-07-24 17:55:44 +0100

    msToHour fun improvement

--- src/utils/msToHour.ts
+++ src/utils/msToHour.ts
@@ -4,5 +4,7 @@ export default (time: number): string => {
     m = ~~((time / 60) % 60),
     h = ~~(time / 60 / 60);
 
-  return `${String(Math.abs(h) % 24).padStart(2, '0')}:${String(m).padStart(2, '0')}:${String(s).padStart(2, '0')}`
+  return h === 0
+    ? `${String(m).padStart(2, '0')}:${String(s).padStart(2, '0')}`
+    : `${String(Math.abs(h) % 24).padStart(2, '0')}:${String(m).padStart(2, '0')}:${String(s).padStart(2, '0')}`;
 }
