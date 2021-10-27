453bdbf6329f6d2b6f929bb4484e38ad5a70f37b
David Amorim <davidfilipeamorim@gmail.com>
2021-10-27 14:43:01 +0100

    Update nodejs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,14 @@ import './Database';
 
 import Client from './structures/Client';
 
+process.on('uncaughtException', (err) => {
+  console.error(err);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.error(err);
+});
+
 const client = new Client();
 
 client.loadCommands();
