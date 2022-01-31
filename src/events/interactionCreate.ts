2d81aea45e3cb3d5c3a3862839a18eecd2b7472d
David Amorim <davidfilipeamorim@gmail.com>
2022-01-31 18:24:16 +0000

    Bug fix

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -12,7 +12,7 @@ export default class InteractionCreate {
 
   async run(interaction: Interaction) {
     if (interaction instanceof AutocompleteInteraction) {
-      if (interaction.channel.type === 1) return;
+      if (!interaction.member) return;
       const cmd = this.client.commands.find(c => c.name === interaction.data.name);
 
       if (!cmd) throw new Error(`Command ${interaction.data.name} does not exist!`);
@@ -37,7 +37,7 @@ export default class InteractionCreate {
       return;
     }
 
-    if (interaction.channel.type === 1) {
+    if (!interaction.member) {
       interaction.createMessage(':frowning: De momento não suporto comandos nas DMs!');
       return;
     }
