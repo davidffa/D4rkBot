c6a13156aacae834d1d9972ad9f7343e9e10c4d4
David Amorim <davidfilipeamorim@gmail.com>
2022-09-02 22:09:49 +0100

    Remake logs cmd

--- src/commands/Settings/logs.ts
+++ src/commands/Settings/logs.ts
@@ -1,9 +1,6 @@
 import Command from '../../structures/Command';
 import Client from '../../structures/Client';
 import CommandContext from '../../structures/CommandContext';
-import { ComponentCollector, MessageCollector } from '../../structures/Collector';
-
-import { Message, ActionRow, ActionRowComponents, ComponentInteraction } from 'eris';
 
 export default class Logs extends Command {
   constructor(client: Client) {
@@ -24,167 +21,74 @@ export default class Logs extends Command {
       return;
     }
 
-    if (!ctx.channel.permissionsOf(this.client.user.id).has('embedLinks')) {
-      ctx.sendMessage({ content: ':x: Preciso da permissão `Inserir Links` para executar este comando', flags: 1 << 6 });
-      return;
-    }
-
     const guildData = this.client.guildCache.get(ctx.guild.id);
-    const welcomeChannel = ctx.guild.channels.get(guildData?.welcomeChatID || '');
-    const byeChannel = ctx.guild.channels.get(guildData?.memberRemoveChatID || '');
 
-    const embed = new this.client.embed()
-      .setColor('RANDOM')
-      .setTitle('Configurar logs')
-      .setDescription(`👋 Canal das mensagens de bem-vindo: \`${welcomeChannel ? `${welcomeChannel.name}` : 'Nenhum'}\`\n\n🚪 Canal das mensagens de saídas de membros: \`${byeChannel ? `${byeChannel.name}` : 'Nenhum'}\``)
-      .setTimestamp()
-      .setFooter(`${ctx.author.username}#${ctx.author.discriminator}`, ctx.author.dynamicAvatarURL());
+    if (!guildData) return;
 
-    const components: ActionRowComponents[] = [
-      {
-        custom_id: 'welcome',
-        style: 2,
-        type: 2,
-        emoji: {
-          id: null,
-          name: '👋'
-        },
-      },
-      {
-        custom_id: 'leave',
-        style: 2,
-        type: 2,
-        emoji: {
-          id: null,
-          name: '🚪'
-        }
+    if (ctx.args[0].toLowerCase() === 'welcome') {
+      const channel = ctx.targetChannels?.[0] ?? ctx.guild.channels.get(ctx.args[1]);
+
+      if (!ctx.targetChannels?.[0] && !ctx.guild.channels.get(ctx.args[1])) {
+        guildData.welcomeChatID = '';
+        await this.client.guildDB.updateOne({
+          guildID: ctx.guild.id
+        }, {
+          $unset: { welcomeChatID: '' }
+        });
+
+        ctx.sendMessage('<a:verificado:803678585008816198> Logs de bem-vindo desativadas!');
+        return;
       }
-    ]
 
-    const row: ActionRow = {
-      type: 1,
-      components
-    }
-
-    const msg = await ctx.sendMessage({ embeds: [embed], components: [row], fetchReply: true }) as Message;
-
-    const welcomeMsgCollector = (): void => {
-      const filter = (m: Message) => m.author.id === ctx.author.id;
-      const collector = new MessageCollector(this.client, ctx.channel, filter, { max: 1, time: 20000 });
-
-      collector.on('collect', async (m) => {
-        if (m.channel.type !== 0) return;
-        msg.delete().catch(() => { });
-
-        if (!guildData) return;
-
-        if (m.content === '0') {
-          guildData.welcomeChatID = '';
-
-          const guildDBData = await this.client.guildDB.findOne({ guildID: m.guildID });
-
-          if (guildDBData) {
-            guildDBData.welcomeChatID = '';
-            guildDBData.save();
-          }
-
-          ctx.sendMessage('<a:verificado:803678585008816198> Logs de bem-vindo desativadas!');
-          return;
-        }
-
-        const channel = m.channel.guild.channels.get(m.channelMentions[0])
-          || m.channel.guild.channels.get(m.content)
-          || m.channel.guild.channels.find(ch => ch.name === m.content)
-          || m.channel.guild.channels.find(ch => ch.name.toLowerCase().includes(m.content.toLowerCase()));
-
-        if (!channel) {
-          m.channel.createMessage(':x: Canal não encontrado');
-          return;
-        }
-
-        guildData.welcomeChatID = channel.id;
-
-        const guildDBData = await this.client.guildDB.findOne({ guildID: m.guildID });
-
-        if (guildDBData) {
-          guildDBData.welcomeChatID = channel.id;
-          guildDBData.save();
-        } else {
-          this.client.guildDB.create({
-            guildID: ctx.guild.id,
-            welcomeChatID: channel.id
-          });
-        }
-
-        ctx.sendMessage(`<a:verificado:803678585008816198> Canal de bem-vindo setado para \`${channel.name}\`.`);
-      });
-    }
-
-    const byeMsgCollector = (): void => {
-      const filter = (m: Message) => m.author.id === ctx.author.id;
-      const collector = new MessageCollector(this.client, ctx.channel, filter, { max: 1, time: 20000 });
-
-      collector.on('collect', async (m) => {
-        if (m.channel.type !== 0) return;
-        msg.delete().catch(() => { });
-        if (!guildData) return;
-
-        if (m.content === '0') {
-          guildData.memberRemoveChatID = '';
-
-          const guildDBData = await this.client.guildDB.findOne({ guildID: m.guildID });
-
-          if (guildDBData) {
-            guildDBData.memberRemoveChatID = '';
-            guildDBData.save();
-          }
-
-          ctx.sendMessage('<a:verificado:803678585008816198> Logs de bem-vindo desativadas!');
-          return;
-        }
-
-        const channel = m.channel.guild.channels.get(m.channelMentions[0])
-          || m.channel.guild.channels.get(m.content)
-          || m.channel.guild.channels.find(ch => ch.name === m.content)
-          || m.channel.guild.channels.find(ch => ch.name.toLowerCase().includes(m.content.toLowerCase()));
-
-        if (!channel) {
-          m.channel.createMessage(':x: Canal não encontrado');
-          return;
-        }
-
-        guildData.memberRemoveChatID = channel.id;
-
-        const guildDBData = await this.client.guildDB.findOne({ guildID: m.guildID });
-
-        if (guildDBData) {
-          guildDBData.memberRemoveChatID = channel.id;
-          guildDBData.save();
-        } else {
-          this.client.guildDB.create({
-            guildID: ctx.guild.id,
-            memberRemoveChatID: channel.id
-          });
-        }
-
-        ctx.sendMessage(`<a:verificado:803678585008816198> Canal de saídas de membros setado para \`${channel.name}\`.`);
-      });
-    }
-
-    const filter = (i: ComponentInteraction) => i.member!.id === ctx.author.id;
-    const collector = new ComponentCollector(this.client, msg, filter, { max: 1, time: 3 * 60 * 1000 });
-
-    collector.on('collect', i => {
-      switch (i.data.custom_id) {
-        case 'welcome':
-          i.editParent({ content: 'Escreva o ID ou o nome do canal para onde as mensagens de bem-vindo irão (Escreva 0 para desativar).', embeds: [], components: [] });
-          welcomeMsgCollector();
-          break;
-        case 'leave':
-          i.editParent({ content: 'Escreva o ID ou o nome do canal para onde as mensagens de saída de membros irão (Escreva 0 para desativar).', embeds: [], components: [] });
-          byeMsgCollector();
-          break;
+      if (!channel) {
+        ctx.sendMessage(':x: Canal não encontrado');
+        return;
       }
-    });
+
+      guildData.welcomeChatID = channel.id;
+
+      await this.client.guildDB.updateOne({
+        guildID: ctx.guild.id
+      }, {
+        $set: { welcomeChatID: '' }
+      }, {
+        upsert: true
+      });
+
+      ctx.sendMessage(`<a:verificado:803678585008816198> Canal de bem-vindo setado para \`${channel.name}\`.`);
+    } else if (ctx.args[0].toLowerCase() === 'leave') {
+      const channel = ctx.targetChannels?.[0] ?? ctx.guild.channels.get(ctx.args[1]);
+
+      if (!ctx.targetChannels?.[0] && !ctx.guild.channels.get(ctx.args[1])) {
+        guildData.memberRemoveChatID = '';
+        await this.client.guildDB.updateOne({
+          guildID: ctx.guild.id
+        }, {
+          $unset: { memberRemoveChatID: '' }
+        });
+
+        ctx.sendMessage('<a:verificado:803678585008816198> Logs de saída desativadas!');
+        return;
+      }
+
+      if (!channel) {
+        ctx.sendMessage(':x: Canal não encontrado');
+        return;
+      }
+
+      guildData.memberRemoveChatID = channel.id;
+
+      await this.client.guildDB.updateOne({
+        guildID: ctx.guild.id
+      }, {
+        $set: { memberRemoveChatID: '' }
+      }, {
+        upsert: true
+      });
+
+      ctx.sendMessage(`<a:verificado:803678585008816198> Canal de mensagens de saída setado para \`${channel.name}\`.`);
+    } else {
+      ctx.sendMessage(`:x: **Usa:** <@${this.client.user.id}> logs <welcome/leave> [#canal]`);
+    }
   }
 }
